6b59739f1da54db91eabb16a3929f10204ace699
Arno Welzel <github@arnowelzel.de>
2025-04-28 22:01:03 +0200

    Update supported WordPress version

--- classes/CommentReplyEmailNotification.php
+++ classes/CommentReplyEmailNotification.php
@@ -3,7 +3,7 @@ namespace CommentReplyEmailNotification;
 
 class CommentReplyEmailNotification
 {
-    const CREN_VERSION = '1.34.0';
+    const CREN_VERSION = '1.35.0';
 
     /**
      * Constructor
